da66f2c42c34ed6595897126010e633d83c06c40
craftsman-fenrir <1257675427@qq.com>
2020-09-17 15:20:59 +0800

    添加了ribbon的核心配置IRule

--- spring-cloud-netflix-demo/ribbon-demo/src/main/java/com/fenrir/RibbonDemoApplication.java
+++ spring-cloud-netflix-demo/ribbon-demo/src/main/java/com/fenrir/RibbonDemoApplication.java
@@ -3,23 +3,32 @@ package com.fenrir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+import com.netflix.loadbalancer.RandomRule;
 
 /**
  * application - spring boot启动类
  * @SpringBootApplication - spring boot启动配置
- * @EnableDiscoveryClient - spring cloud配置，曾经支持任何注册中心
- * 现在eureka已经不支持了，因为@EnableEurekaClient内部不在注解@EnableDiscoveryClient
  * @EnableEurekaClient - eureka配置，把该服务配置为eureka注册中心的客户端
+ * @RibbonClient - ribbon核心配置，可选，定义均衡负载的规则（默认ZoneAvoidanceRule）和微服务名字（不写用从配置获取默认名）
+ * RoundRobinRule 轮询
+ * RandomRule 随机
+ * AvailabilityFilteringRule 会优先选择比较健康的微服务（短路或连接数量过多的会少访问）
+ * WeightedResponseTimeRule 根据响应时间来决定优先级
+ * RetryRule 先轮询，获取失败时会在指定时间内重试，多次失败则不会再获取该服务
+ * BestAvailableRule 会先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，然后选择一个并发量最小的服务
+ * ZoneAvoidanceRule 默认规则，复合判断Server所在区域的性能和Server的可用性选择服务器
  * @author fenrir
  *
  */
 @SpringBootApplication
-//@EnableDiscoveryClient
 @EnableEurekaClient
+@RibbonClient(configuration = RandomRule.class)
 public class RibbonDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RibbonDemoApplication.class, args);
 	}
-	
+
 }
