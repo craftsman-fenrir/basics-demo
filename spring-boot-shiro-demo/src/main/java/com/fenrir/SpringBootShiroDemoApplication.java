f0092e8532a6c009e7ed9ed9a0ba3483b96a7faa
craftsman-fenrir <1257675427@qq.com>
2021-01-05 09:49:48 +0800

    创建了shiro的使用demo

---
+++ spring-boot-shiro-demo/src/main/java/com/fenrir/SpringBootShiroDemoApplication.java
@@ -0,0 +1,19 @@
+package com.fenrir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * application - spring boot启动类
+ * @SpringBootApplication - spring boot启动配置
+ * @author fenrir
+ *
+ */
+@SpringBootApplication
+public class SpringBootShiroDemoApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(SpringBootShiroDemoApplication.class, args);
+    }
+	
+}
